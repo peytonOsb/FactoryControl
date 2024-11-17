a852ec222959b533e35524a5bb4240eaafe757ec
peytonOsb <peytontosborn@gmail.com>
2024-11-16 22:16:40 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,8 +5,4 @@ local master = mc:new("7",-100,100)
 local slave = mc:new("8",-100,100)
 local slave2 = mc:new("9",-100,100)
 
-master:setSlave(slave,true)
-master:setSlave(slave,false)
-
-master:run(20)
-master:run(70,true)
+master:run(20,false)
